e275702b3decb90b93a54e7d91072e90e1c9c516
Carl NOMBO <carl.nombo@gmail.com>
2019-06-08 19:39:21 +0000

    + Menu

--- js/menu.js
+++ js/menu.js
@@ -15,6 +15,9 @@ $('#boutonPlay').on('click',function() {
 
 	var wayLayer = new Konva.Layer();
 	var starLayer = new Konva.Layer();
+	var imgLayer1 = new Konva.Layer();
+	var imgLayer2 = new Konva.Layer();
+	var imgLayer3 = new Konva.Layer();
 
 	var stageMenu = new Konva.Stage({
 	    container: 'menu',   // id of container <div>
@@ -28,7 +31,7 @@ $('#boutonPlay').on('click',function() {
 
 	    var simpleText = new Konva.Text({
         x: width/2-width/5,
-        y: height/5,
+        y: height/10,
         text: 'Apprendre à compter',
         fontSize: 60,
         fontFamily: 'comic',
@@ -41,23 +44,39 @@ $('#boutonPlay').on('click',function() {
 	  layerTittle.add(simpleText);
 	  stageMenu.add(layerTittle);
 
-	/*var wayImg = new Image();
+	var wayImg = new Image();
 	wayImg.onload = function() {
 		var way = new Konva.Image({
-			x:100,
-			y:100,
+			x:(width/2-width/3)/2,
+			y:height/4,
 			image: wayImg,
 			width:width/4,
 			height:height/4
 		});
+		wayLayer.add(way);
+		stageMenu.add(wayLayer);		
 	};
-	wayImg.src='';*/
+	wayImg.src='img/scene1.png';
+
+	var imgObj1 = new Image();
+	imgObj1.onload = function() {
+		var img = new Konva.Image({
+			x:2*(width/2-width/6-8),
+			y:height/4,
+			image: imgObj1,
+			width:width/4,
+			height:height/4
+		});	
+		imgLayer1.add(img);
+		stageMenu.add(imgLayer1);
+	};
+	imgObj1.src='img/scene2.png';
 
 	var starImg = new Image();
 	starImg.onload = function() {
 		var star = new Konva.Image({
-			x:width/2-width/5,
-			y:height/2,
+			x:(width/3+width/30),
+			y:(height/2-height/15),
 			image: starImg,
 			width:width/4,
 			height:height/4
@@ -67,6 +86,33 @@ $('#boutonPlay').on('click',function() {
 	};
 	starImg.src='img/menuStar.svg';
 
+	var imgObj2 = new Image();
+	imgObj2.onload = function() {
+		var img = new Konva.Image({
+			x:(width/2-width/3)/2,
+			y:height/2+height/8,
+			image: imgObj2,
+			width:width/4,
+			height:height/4
+		});	
+		imgLayer2.add(img);
+		stageMenu.add(imgLayer2);
+	};
+	imgObj2.src='img/scene3.png';
+
+	var imgObj3 = new Image();
+	imgObj3.onload = function() {
+		var img = new Konva.Image({
+			x:2*(width/2-width/6-8),
+			y:height/2+height/8,
+			image: imgObj3,
+			width:width/4,
+			height:height/4
+		});	
+		imgLayer3.add(img);
+		stageMenu.add(imgLayer3);
+	};
+	imgObj3.src='img/scene4.png';		
 	//wayLayer.add(way);
 	
 	//stage.add(wayLayer);
