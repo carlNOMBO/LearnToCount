5b8dee55928e7c84e80ecb1ff777caa47d391540
Carl NOMBO <carl.nombo@gmail.com>
2019-06-22 16:02:02 +0100

    +Utiliser la grille avec des images

--- js/scene1.js
+++ js/scene1.js
@@ -3,37 +3,20 @@ var height=window.innerHeight;
 var widthZoneJeu = width-width/4;
 var heightZonJeu = height-height/4;
 var xJeu = width/2-widthZoneJeu/2;
-var yJeu = height/2-heightZonJeu/2;
+var yJeu = 0;
 var tailleCarreau = widthZoneJeu/6;
+var widthCareau = widthZoneJeu/6;
+var heightCareau = heightZonJeu/4;
+var number=0;
+if(widthCareau>heightCareau) number=heightCareau;
+else number=widthCareau;
 
-$("body").css({
-	"background-color":"#88001b"
-});
-
-$("#scene1").css({
-	"background-color":"88001b"
-});
 
 var stage = new Konva.Stage({
     container: 'scene1',   
     width: width,
     height: height	
 });
-
-var zoneLayer=new Konva.Layer();
-
-var zoneJeu = new Konva.Rect({
-    x: xJeu,
-    y: yJeu,
-    width: widthZoneJeu,
-    height: heightZonJeu,
-    fill: '#8D9590'
-    /*stroke: 'black',
-    strokeWidth: 4*/	
-});
-
-zoneLayer.add(zoneJeu);
-stage.add(zoneLayer);
 
 
 //Gestion de la grille
@@ -45,7 +28,7 @@ var carreauxGroup = new Konva.Group({
 });
 
 
-function loadImages(source,callback) {
+/*function loadImages(source,callback) {
 
 	// get num of sources
 	var image = new Image();
@@ -56,37 +39,108 @@ function loadImages(source,callback) {
 }
 
 var k=1;
-var carreau;
-for(var i=0; i<4; i++)
-{
-	for(var j=0; j<3; j++)
-	{
-		//loadImages('img/'+k+'.png',function(image) {
 
-			carreau = new Konva.Rect({
-		      x: i * tailleCarreau,
-		      y: j * tailleCarreau ,
-		      width: tailleCarreau,
-		      height: tailleCarreau,
-		      name: ""+i,
-			 // fillPatternImage:image,
-			 // fillPatternRepeat:'no-repeat',		      
+for(var i=0; i<6; i++)
+{
+	for(var j=0; j<5; j++)
+	{
+
+      var simpleText = new Konva.Text({
+        x: i * widthCareau,
+        y: j * heightCareau,
+        text: ''+k,
+        fontSize: number,
+        fontFamily: 'Calibri',
+        fill: 'green'
+      });
+
+			var carreau = new Konva.Rect({
+		      x: i * widthCareau,
+		      y: j * heightCareau ,
+		      width: widthCareau,
+		      height: heightCareau,
+		      name: ""+i,		      
 		      fill: "white",
 		      stroke: 'black',
 		      strokeWidth: 4		
 			});
 			carreauxGroup.add(carreau);				
-		//});
+      carreauxGroup.add(simpleText);
 		k++;
 	}
 }
 
 carreauxLayer.add(carreauxGroup);
-stage.add(carreauxLayer);
+stage.add(carreauxLayer);*/
+
+
+//////Utilisation des images///////////////////////////////////////////////////////
+
+      function loadImages(source,callback) {
+
+        // get num of sources
+        var image1 = new Image();
+            image1.onload = function(){
+              callback(image1);
+            };
+            image1.src=source;        
+      }     
+
+      function draw(images) {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+
+        var layer = new Konva.Layer();        
+
+
+        var mesRect = {};
+        var groupRect = new Konva.Group({
+          x:xJeu,
+          y:yJeu
+        })
+var k=0;
+for(var j=0; j<=2; j++)
+{
+      for(var i=0; i<=2; i++)
+      { 
+        mesRect[k] = new Konva.Rect({
+          x: i * widthCareau,
+          y: j*heightCareau,
+          width: widthCareau,
+          height:heightCareau,
+          //sides: 5,
+          //radius: 70,
+          fillPatternImage: images[k],
+          fillPatternOffset: { x: 0, y: 0 },
+          fillPatternRepeat:'no-repeat',
+          stroke: 'black',
+          strokeWidth: 4
+        });
+
+        groupRect.add(mesRect[k]);
+        k++;
+      }
+}     k=0;
+        layer.add(groupRect);
+        stage.add(layer);
+      }
+ 
+ var imgs = [];
+
+ for(var i=1; i<=10; i++){
+      loadImages('img/'+i+'.png',function(image) {
+        imgs.push(image)
+        //draw(images);
+      });
+}
+
+loadImages('img/play.png',function(image){
+  draw(imgs);
+});
 
 
 
-//Gestion du personnage
+//Gestion du personnage////////////////////////////////////////////////////////////////////////////////////
 var persolayer = new Konva.Layer();
      var container = stage.container();
 
@@ -102,11 +156,11 @@ var persolayer = new Konva.Layer();
 var perso = new Image();
     perso.onload = function() {
       var play = new Konva.Image({
-        x: (xJeu+tailleCarreau/2)-(tailleCarreau/8),
-        y: (yJeu+tailleCarreau/2)-(tailleCarreau/4),
+        x: (xJeu+widthCareau/2)-(widthCareau/8),
+        y: (yJeu+heightCareau/2)-(heightCareau/4),
         image: perso,
-        width: tailleCarreau/4,
-        height: tailleCarreau/2,
+        width: widthCareau/4,
+        height: heightCareau/2,
         shadowOffsetX: 20,
         shadowOffset: 25,
         shadowBlur : 40
@@ -116,15 +170,20 @@ var perso = new Image();
  		stage.add(persolayer);
 
       container.addEventListener('keydown', function(e) {
-        if (e.keyCode === gauche) {        	
-          play.x(play.x() - tailleCarreau);
-        } else if (e.keyCode === haut) {        	
-          play.y(play.y() - tailleCarreau);
-        } else if (e.keyCode === droite) {
-          play.x(play.x() + tailleCarreau);
-        } else if (e.keyCode === bas) {
-          play.y(play.y() + tailleCarreau);
-        } else {
+        if (e.keyCode === gauche) {
+                                   	
+          play.x(play.x() - widthCareau);
+        } 
+        else if (e.keyCode === haut) {        	
+          play.y(play.y() - heightCareau);
+        } 
+        else if (e.keyCode === droite) {          
+          play.x(play.x() + widthCareau);
+        } 
+        else if (e.keyCode === bas) {
+          play.y(play.y() + heightCareau);
+        } 
+        else {
           return;
         }
         e.preventDefault();
--- js/test.js
+++ js/test.js
@@ -1,64 +1,172 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-var testLayer = new Konva.Layer();
+
 var stage = new Konva.Stage({
-    container:'container',
-    width:width,
-    height:height
+		container:'container',
+		width:width,
+		height:height
 });
 
-      function loadImages(source,callback) {
+ var imgLayer = new Konva.Layer();
+var imgGroup= new Konva.Group({
+	x:10,
+	y:10
+});
 
+for(var i=1; i<4; i++)
+{
+	var carreau = new Image();
+			carreau.onload = function() {
+				var nombre = new Konva.Image({
+					x:  i*100,
+					//y:  300 ,
+					image: carreau,
+					width: 100,
+					height: 100,
+					stroke: 'black',
+					strokeWidth: 4,
+					draggable:true
+				});
+			imgGroup.add(nombre);
+			//if(i>=4) 
+			{
+				imgLayer.add(imgGroup);
+				 
+			}                           
+			};
+	carreau.src = 'img/'+i+'.png';
+}
+stage.add(imgLayer);
+
+
+      function loadImages1(sources, callback) {
+        var images = {};
+        var loadedImages = 0;
+        var numImages = 0;
         // get num of sources
-        var image1 = new Image();
-            image1.onload = function(){
-              callback(image1);
-            };
-            image1.src=source;        
+        for (var src in sources) {
+          numImages++;
+        }
+        for (var src in sources) {
+          images[src] = new Image();
+          images[src].onload = function() {
+            if (++loadedImages >= numImages) {
+              callback(images);
+            }
+          };
+          images[src].src = sources[src];
+        }
       }
 
-      function draw(images) {
-        var width = window.innerWidth;
-        var height = window.innerHeight;
+			function loadImages(source,callback) {
 
-        var layer = new Konva.Layer();        
+				// get num of sources
+				var image1 = new Image();
+						image1.onload = function(){
+							callback(image1);
+						};
+						image1.src=source;        
+			}			
 
-        var patternPentagon = new Konva.RegularPolygon({
-          x: 220,
-          y: stage.height() / 2,
-          sides: 5,
-          radius: 70,
-          fillPatternImage: images,
-          //fillPatternOffset: { x: -220, y: 70 },
-          stroke: 'black',
-          strokeWidth: 4,
-          draggable: true
-        });
+			function draw(images) {
+				var width = window.innerWidth;
+				var height = window.innerHeight;
+
+				var layer = new Konva.Layer();        
 
 
-        layer.add(patternPentagon);
-        stage.add(layer);
-      }
+				var mesRect = {};
+				var groupRect = new Konva.Group({
+					x:100,
+					y:100
+				})
+var k=0;
+for(var j=0; j<=2; j++)
+{
+			for(var i=0; i<=2; i++)
+			{	
+				mesRect[k] = new Konva.Rect({
+					x: i*100,
+					y: j*100,
+					width: 100,
+					height:100,
+					//sides: 5,
+					//radius: 70,
+					fillPatternImage: images[k],
+					fillPatternOffset: { x: 0, y: 0 },
+					fillPatternRepeat:'no-repeat',
+					stroke: 'black',
+					strokeWidth: 4,
+					draggable: true
+				});
+
+				groupRect.add(mesRect[k]);
+				k++;
+			}
+}			k=0;
+				layer.add(groupRect);
+				stage.add(layer);
+			}
  
-      loadImages('img/5.png',function(images) {
-        draw(images);
-      });
+ var imgs = [];
 
-for(var i=1; i<2; i++)
+ for(var i=1; i<=10; i++){
+			loadImages('img/'+i+'.png',function(image) {
+				imgs.push(image)
+				//draw(images);
+			});
+}
+
+loadImages('img/play.png',function(image){
+	draw(imgs);
+});
+
+      /*var sources = {
+        darthVader: 'img/5.png',
+        yoda: 'img/5.png'
+      };*/
+
+      /*loadImages1(sources, function(images) {
+        draw(images);
+      });*/
+
+
+
+
+/*var testLayer = new Konva.Layer();
+
+testLayer.size({
+	width:500,
+	height:500
+})
+
+var group = new Konva.Group({
+	x:10,
+	y:10
+});
+
+/*for(var i=1; i<5; i++)
 {
 loadImages('img/'+i+'.png',function(image){
-  var monRec = new Konva.Rect({
-   
-    width:100,
-    height:100,    
-    fillPatternImage:image,
-    fillPatternRepeat:'no-repeat',    
-    fillPatternOffset: { x: -20, y: -2 }
-  });    
+	var recLayer = new Konva.Layer();
+	var monRec = new Konva.Rect({
+		x:50*i,
+		y:0,
+		width:100,
+		height:100,    
+		fillPatternImage:image,
+		fillPatternRepeat:'no-repeat',    
+		fillPatternOffset: { x: -20, y: -2 },
+		draggable:true
+	});    
 
-  testLayer.add(monRec);
-  stage.add(testLayer);
+	//group.add(monRec);
+	recLayer.add(monRec);
+	stage.add(recLayer);
 });
-}  
-  
+}*/
+/*testLayer.add(group);
+stage.add(testLayer); 
+testLayer.draw(); */
+	
